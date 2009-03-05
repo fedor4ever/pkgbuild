0d24e6e7392514db7b62f31934af3f025f1bc19d
dariosestito <dariosestito@d070750.prod.ad.symbian.intra>
2009-03-05 18:20:11 +0000

    Various changes

--- common/hlm.bat
+++ common/hlm.bat
@@ -2,6 +2,6 @@
 setlocal
 if not defined HELIUM_HOME set HELIUM_HOME=%~dp0..\..\..\..\helium
 echo %HELIUM_HOME%
-call ..\..\build\env.bat
+rem call ..\..\build\env.bat
 call %HELIUM_HOME%\hlm.bat %*
 endlocal     
