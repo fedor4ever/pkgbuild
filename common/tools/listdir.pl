6d4014648d3649df88304c50440d5e66dafaff8c
Shabe Razvi <shaber@symbian.org>
2009-04-30 16:09:08 +0100

    Strip drive and dir root from listing

--- common/tools/listdir.pl
+++ common/tools/listdir.pl
@@ -3,12 +3,15 @@ use strict;
 
 my $dir      = shift or die "Usage: $0 <dir> \n";   #  provided dir to traverse
 my $filelist = [];
+my $init = $dir = lc($dir);
+$init =~ s{\\}{\\\\};
 
 # fwd declaration to prevent warning
 sub recursedir($$);
 
 # run recurse and print
 recursedir ($dir, $filelist);
+
 print $_, "\n" for(@$filelist);
 
 sub recursedir($$) {
@@ -19,15 +22,14 @@ sub recursedir($$) {
   if(opendir(DIR, "$dir")) {
     #  list dir
     for my $file(grep { !/^\./ } readdir DIR) {
-      if(-d "$dir\\$file") {
+      if(-d "$dir/$file") {
         #  traverse subdirs
-        recursedir("$dir\\$file", $list);
+        recursedir("$dir/$file", $list);
       }
-      elsif(-f "$dir\\$file") {
-        #  if file then swap (any present) fwd to bkslash and add to list        
-        $dir   =~s/\//\\/;
-        $file  =~s/\//\\/;
-        push @$list, "$dir\\$file";
+      elsif(-f "$dir/$file") {
+        my $formatted = lc($dir)."/".lc($file);
+        $formatted =~ s!$init/!!;
+        push @$list, $formatted;
       }
     }
     closedir DIR;
