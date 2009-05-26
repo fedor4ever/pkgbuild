2476cfc2433c2fdcafb605909b0b62d4a60a143e
Simon Howkins <simonh@symbian.org>
2009-05-26 12:42:51 +0100

    Added license header

--- common/tools/populateziptemplate.pl
+++ common/tools/populateziptemplate.pl
@@ -1,5 +1,19 @@
+#!perl -w
+# Copyright (c) 2009 Symbian Foundation Ltd
+# This component and the accompanying materials are made available
+# under the terms of the License "Eclipse Public License v1.0"
+# which accompanies this distribution, and is available
+# at the URL "http://www.eclipse.org/legal/epl-v10.html".
+#
+# Initial Contributors:
+# Symbian Foundation Ltd - initial contribution.
+# 
+# Contributors:
+#
+# Description:
+# Populates the template for packaging src and binaries in the build
+
 use strict;
-use warnings;
 
 use FindBin;
 use lib "$FindBin::Bin/lib";
