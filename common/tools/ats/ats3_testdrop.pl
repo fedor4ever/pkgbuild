f045f723263ca2829ff892a9f5dec69cd15a3323
Mike Kinghan <mikek@symbian.org>
2009-05-28 14:55:09 +0100

    Adding license boilerplate to perl scripts.

--- common/tools/ats/ats3_testdrop.pl
+++ common/tools/ats/ats3_testdrop.pl
@@ -1,5 +1,20 @@
 #!/usr/bin/perl
 
+# Copyright (c) 2009 Symbian Foundation Ltd
+# This component and the accompanying materials are made available
+# under the terms of the License "Eclipse Public License v1.0"
+# which accompanies this distribution, and is available
+# at the URL "http://www.eclipse.org/legal/epl-v10.html".
+#
+# Initial Contributors:
+# Symbian Foundation Ltd - initial contribution.
+#   Mike Kinghan, mikek@symbian.org
+#
+# Contributors:
+#
+# Description:
+#   This is a tool for sending an ATS3 testdrop to an ATS3 server.
+
 use strict;
 use Getopt::Long;
 use File::Copy;
