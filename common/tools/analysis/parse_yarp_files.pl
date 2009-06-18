2a68dbbf91ecda550a374909c01bd65376a16244
Dario Sestito <darios@symbian.org>
2009-06-18 10:05:42 +0100

    Add missing file pare_yarp_files.pl

---
+++ common/tools/analysis/parse_yarp_files.pl
@@ -0,0 +1,38 @@
+
+my @asYarpFiles = ();
+open(FILE, $ARGV[0]);
+while ( <FILE> )
+{
+	if ( m/[^,]*,([^,]*)/ )
+	{
+		my $sFile = $1;
+		chomp $sFile;
+#		print "$sFile\n";
+		push(@asYarpFiles, $sFile);
+	}
+}
+close(FILE);
+my $nYarpFiles = scalar(@asYarpFiles);
+#print "Found $nYarpFiles files to parse\n";
+
+my $nTotalRecipeErrors = 0;
+for my $nYarpFile (@asYarpFiles)
+{
+	my $nRecipeErrors = 0;
+#	print "Parsing file $nYarpFile...\n";
+	open(FILE, $nYarpFile);
+	while ( <FILE> )
+	{
+		if ( m/Overall attempts:\s*(\d+),(\d+)/ )
+		{
+			print "Attempt: $1 Errors: $2\n";
+			$nRecipeErrors = $2;
+		}
+	}
+	close(FILE);
+#	print "Recipe errors in $nYarpFile: $nRecipeErrors\n";
+	$nTotalRecipeErrors += $nRecipeErrors;
+}
+#print "Total recipe errors across all files: $nTotalRecipeErrors\n";
+
+print "$nTotalRecipeErrors";
